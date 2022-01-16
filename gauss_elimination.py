ff7b5622e96943852f5a32a5a1f34c1dbc178f1c
Gregorio <giorgino.greg@gmail.com>
2022-01-16 20:02:49 +0100

    Initialized gauss elimination function

--- gauss_elimination.py
+++ gauss_elimination.py
@@ -1,3 +1,12 @@
-
+from numpy import *;
+from matrix import multipliers;
 def gauss_elimination(M, b):
+    """Compute an array of equations starting from a coefficients matrix and a vector of results"""
+    [n,m] = shape(M);
+    for i in range(1, n):
+        multipliers = multipliers(M[i:,:])
+        for cols in range(1,m):
+            M[i][cols] += multipliers[i] * M[i][cols]
+        
+
     return
